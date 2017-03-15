1c1cd835cbd8e408037a29d8df6e34ab8a7dffef
Jc Tecklenburg <jt9816a@student.american.edu>
2017-03-15 11:48:38 -0400

    works getting tweets, but can't save to file because of dumb characters

--- bot.py
+++ bot.py
@@ -1,9 +1,104 @@
+# coding: utf-8
+# Copyright (c) 2015–2016 Molly White
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+ 
+import os
 import tweepy
-
 from secrets import *
+from time import gmtime, strftime
+import csv
+import markovify
 
-auth = tweepy.OAuthHandler(C_KEY, C_SECRET)  
-auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)  
-api = tweepy.API(auth)  
 
-print("HELLO WORLD2")
+# ====== Individual bot configuration ==========================
+bot_username = 'pikeroROBO'
+logfile_name = bot_username + ".log"
+
+# ==============================================================
+def initialize():
+    global api
+    auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
+    auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+    api = tweepy.API(auth)
+
+def get_all_tweets(screen_name):
+    #Twitter only allows access to a users most recent 3240 tweets with this method
+    alltweets = []
+    #make initial request for most recent tweets (200 is the maximum allowed count)
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+    alltweets.extend(new_tweets)
+    oldest = alltweets[-1].id - 1
+    while len(new_tweets) > 0:
+        print ("getting tweets before %s" % (oldest))
+        new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+        alltweets.extend(new_tweets)
+        oldest = alltweets[-1].id - 1
+ 
+        print ("...%s tweets downloaded so far" % (len(alltweets)))
+ 
+    outtweets = [[tweet.text] for tweet in alltweets]
+    outtweets = outtweets
+ 
+    with open('%s_tweets.csv' % screen_name, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(outtweets)
+
+def create_tweet():
+	#"""Create the text of the tweet you want to send."""
+	# Replace this with your code!
+	get_all_tweets("pikero24")
+	with open("Eric_C_Wilder_tweets.csv", "r") as f:
+		text = f.read()
+	text = [word for word in text.split() if not "@" in word if not "#" in word]
+	text = " ".join(text)
+	textModel = markovify.Text(text)
+	fakeTweet = textModel.make_short_sentence(140)
+	text = fakeTweet
+	return text
+
+
+def tweet(text):
+    """Send out the text as a tweet."""
+    # Twitter authentication
+    auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
+    auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+    api = tweepy.API(auth)
+
+    # Send the tweet and log success or failure
+    try:
+        api.update_status(text)
+    except tweepy.error.TweepError as e:
+        log(e.message)
+    else:
+        log("Tweeted: " + text)
+
+
+def log(message):
+    """Log message to logfile."""
+    path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    with open(os.path.join(path, logfile_name), 'a+') as f:
+        t = strftime("%d %b %Y %H:%M:%S", gmtime())
+        f.write("\n" + t + " " + str(message))
+
+
+if __name__ == "__main__":
+	initialize()
+	tweet_text = create_tweet()
+	tweet(tweet_text)
