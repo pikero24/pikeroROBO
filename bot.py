c839194f7ded0c8a17865cc70f365c94b9928bf7
Jc Tecklenburg <jt9816a@student.american.edu>
2017-03-15 12:20:50 -0400

    git being git

--- bot.py
+++ bot.py
@@ -18,7 +18,10 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
- 
+
+#TODO Check if new tweets before regenerating csv
+
+
 import os
 import tweepy
 from secrets import *
